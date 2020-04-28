3c2e0d1df56f4fe6dcf4f21b84291e3945aebe5a
steve <samurphy89@gmail.com>
2020-04-28 15:43:47 -0400

    updated file structure a little

--- GeneralJudgementalBot/src/GeneralJudgementalBot.js
+++ GeneralJudgementalBot/src/GeneralJudgementalBot.js
@@ -5,18 +5,18 @@ const client = new Discord.Client()
 client.login(config.token)
 
 client.on('ready', () => {
-    client.user.setPresence({ activity: { type: 'WATCHING', name: ' and Silently Judging' }})
+	client.user.setPresence({ activity: { type: 'WATCHING', name: ' and Silently Judging' }})
 });
 
 client.on('message', (receivedMessage) => {
 	if (receivedMessage.author == client.user) return;
-	
+
 	if(receivedMessage.channel.name !== 'music'){
 		if((receivedMessage.author.username == 'Rythm' || receivedMessage.author.username == 'Rythm 2')
 			&& !receivedMessage.content.includes('Searching'))
-		
+
 			receivedMessage.channel.send(varyResponses());
-    };    
+	};    
 });
 
 let varyResponses = () => {
