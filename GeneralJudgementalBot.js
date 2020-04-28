a63b728176a61e99105a36aa851a7665c99eb8bf
steve <samurphy89@gmail.com>
2020-04-28 10:24:19 -0400

    fixed my stupidity

--- GeneralJudgementalBot.js
+++ GeneralJudgementalBot.js
@@ -4,12 +4,12 @@ const client = new Discord.Client()
 // Get your bot's secret token from:
 // https://discordapp.com/developers/applications/
 // Click on your application -> Bot -> Token -> "Click to Reveal Token"
-bot_secret_token = "NzA0NDc2MDUyNDc4NjIzNzg1.Xqd3Qg.OA467Ph2IUdr3zhihDM1dqW-ZUk"
+bot_secret_token = "[secret_token]"
 
 client.login(bot_secret_token)
 
 client.on('message', (receivedMessage) => {
-    var generalChannel = client.channels.cache.get('457989533611458592')
+    var generalChannel = client.channels.cache.get('[client_channel]')
     if (receivedMessage.author == client.user) {
         return
     }
